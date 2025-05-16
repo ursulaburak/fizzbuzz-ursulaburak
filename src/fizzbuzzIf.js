4bb329f0d141ee5fb457bc2efa3e9d7c56452f5c
ursulaburak <burakkavcilar@gmail.com>
2025-05-16 13:57:23 +0300

    FizzBuzz test edildi.

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,14 @@
-function fizzbuzzIf(num) {
+function fizzbuzzIf(n) {
     // Kodlar buraya gelecek
+    if(n % 3 === 0 && n % 5 === 0) {
+        return 'FizzBuzz';
+    }   else if (n % 3 === 0) {
+        return 'Fizz';
+    }   else if (n % 5 === 0) {
+        return 'Buzz';
+    }   else   {
+        return n;
+    }
 }
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,15 @@
-function fizzbuzzSwitch(num) {
+function fizzbuzzSwitch(n) {
     // Kodlar buraya gelecek
+    switch (true) {
+    case n % 15 === 0:
+      return 'FizzBuzz';
+    case n % 3 === 0:
+      return 'Fizz';
+    case n % 5 === 0:
+      return 'Buzz';
+    default:
+      return n;
+  }
 }
 
 module.exports = fizzbuzzSwitch;
